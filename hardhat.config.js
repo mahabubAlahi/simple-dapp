dac28883962e361d6bbac58748dafda2c5bb447f
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-09-24 11:40:28 +0600

    refactor: hardhat config file updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+require('hardhat-deploy');
+
 module.exports = {
   solidity: "0.8.17",
+  namedAccounts: {
+    deployer: 0
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.INFURA_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/'
+    }
+  },
 };
